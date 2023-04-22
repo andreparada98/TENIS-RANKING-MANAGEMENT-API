3a2ee360c7ec3bec41dada42f6bcff0b007efe99
Andre Parada <andreparada98@gmail.com>
2023-04-22 02:45:52 -0300

    feat: add player interface and dto

---
+++ src/players/dto/player.dto.ts
@@ -0,0 +1,8 @@
+export class PlayerDTO {
+  email: string;
+  phoneNumber: string;
+  name: string;
+  ranking: string;
+  positionRanking: number;
+  urlPhotoPlayer: string;
+}
---
+++ src/players/interfaces/player.interface.ts
@@ -0,0 +1,11 @@
+import { Document } from 'mongoose';
+
+export interface Player extends Document {
+  readonly _id: string;
+  readonly phoneNumber: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  positionRanking: number;
+  urlPhotoPlayer: string;
+}
