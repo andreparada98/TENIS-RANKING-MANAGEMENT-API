d8f87f2f4af4926353a6fa4509195cebadbbbb72
Andre Parada <andreparada98@gmail.com>
2023-04-22 02:45:21 -0300

    chore: add mongo connection string and .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { PlayersModule } from './players/players.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@agendamentocovid19.edb2x.mongodb.net/smartranking?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      },
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@agendamentocovid19.edb2x.mongodb.net/smartranking`,
     ),
+    PlayersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
