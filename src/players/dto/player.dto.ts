242cb1378e40537975faf54bdc285b7e03e2aac4
Andre Parada <andreparada98@gmail.com>
2023-04-25 01:14:59 -0300

    feat: add validation in post req

--- src/players/dto/player.dto.ts
+++ src/players/dto/player.dto.ts
@@ -1,6 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class PlayerDTO {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   phoneNumber: string;
+
   name: string;
   ranking: string;
   positionRanking: number;
